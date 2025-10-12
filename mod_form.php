3841fffc1c9bd70786f11a328ae0c4dcecc8ce13
Tim Helland <business@mr-math.com>
2025-10-12 16:45:19 -0700

    Remove unused property in mod settings.

--- mod_form.php
+++ mod_form.php
@@ -7,9 +7,6 @@ class mod_menteesummary_mod_form extends moodleform_mod {
 
         $this->standard_intro_elements();
 
-        $mform->addElement('advcheckbox', 'showinactive', get_string('showinactive', 'menteesummary'));
-        $mform->setDefault('showinactive', 0);
-
         $this->standard_coursemodule_elements();
         $this->add_action_buttons();
     }
