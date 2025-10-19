d4c7dfd1acca75573dd038a4a8b308ec714c8319
Tim Helland <business@mr-math.com>
2025-10-19 14:27:42 -0700

    Update version number.

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_menteesummary';
-$plugin->version   = 2025100501;
+$plugin->version   = 2025101901;
 $plugin->requires  = 2024052700;   // Moodle 4.5+.
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.9';
+$plugin->release   = '1.0';
